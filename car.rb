5fdf4ed2dc26248f26ef8eba2f387264eb451441
abdhass <hassgt@gmail.com>
2014-09-11 13:52:31 +0100

    Updated CarClass Def Added CarDealer Class
    We have cars and car dealership centres.

--- car.rb
+++ car.rb
@@ -1,35 +1,28 @@
 class Car
-	attr_accessor :make
-	attr_accessor :cost
-	attr_accessor :color
-	attr_accessor :sold
-	attr_accessor :reg
-	@@count = 0
-	@@instances = []
-
-	def initialize(reg, make, cost, color)
-	      @make=make
-	      @cost=cost
-	      @color=color
-	      @reg = reg
-	      @@count += 1
-	      @@instances << self
-	end
-
-	def sold?
-		@sold == 'Y' 
-	end
-
-	def self.all
-		puts @@instances.empty? ? "No cars" : @@instances.inspect
-	end
-	def self.count
-		@@count
-	end
-	def self.instances
-		return @@instances
-	end
-
+  attr_accessor :reg, :make, :cost
+  def initialize(reg, make, cost)
+    @reg = reg
+    @make = make
+    @cost = cost
+  end
+  def description
+  puts "This car is a #{@make} and costs #{@cost}"
+  end
 end
 
-#beamer = Car.new('12377', 'bmw', '1000', 'blue')
+class CarDealer
+    @@cars = []
+  def newCar(reg, make, cost)
+    @@cars << Car.new(reg, make, cost)
+  end
+
+def show_cars
+    @@cars.each {|car| car.description} 
+  end
+end
+
+#londonBranch = CarDealer.new
+#londonBranch.newCar("LX14", "Ford", "1200")
+#londonBranch.newCar("LX13", "VW", "1200")
+#londonBranch.newCar("LX12", "Merc", "1200")
+#londonBranch.show_cars
