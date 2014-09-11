5dd18ca961ac2a0598497fe0bf5e602dd23dd5be
abdhass <hassgt@gmail.com>
2014-09-11 17:22:44 +0100

    Fixed object creation problem

--- main.rb
+++ main.rb
@@ -1,18 +1,15 @@
 require_relative 'car'
 
-def mainmenu
+def mainmenu(londonBranch)
+	main_menu = "|||please type in a menu number|||
+	1 - Add a new car
+	2 - Add customer
+	3 - Cars for sale
+	4 - view car details
+	9 - **exit the program**"
 
-main_menu = "|||please type in a menu number|||
-1 - Add a new car
-2 - Add customer
-3 - Cars for sale
-4 - view car details
-
-9 - **exit the program**"
-
-puts main_menu
-
-menu_choice = gets.chomp
+	puts main_menu
+	menu_choice = gets.chomp
 
 	case menu_choice.to_i
 	when 1
@@ -20,37 +17,30 @@ menu_choice = gets.chomp
 		reg = gets.chomp
 		puts "Make of car?"
 		make = gets.chomp
-		puts "Colour ?"
-		colour = gets.chomp
 		puts "Whats the cost of #{make}?"
 		cost = gets.chomp
-		#puts reg+"="+"Car.new('#{reg}','#{make}','#{cost}','#{colour}')"
-		car = Car.new(reg,make,cost,colour)
-		#eval("return #{reg} = Car.new('#{reg}','#{make}','#{cost}','#{colour}')")
+		londonBranch.newCar("#{reg}","#{make}","#{cost}")
 	when 3
-		puts Car.all
+		londonBranch.show_car
 		puts "hit enter for menu"
 		gets.chomp
 	when 4
 		puts "reg number?"
 		regnum = gets.chomp
-		Car.instances.each do |a| 
-			if a.reg == regnum
-			puts "Car Reg: #{a.reg}" 
-			newc = a 
-			newc.make
-		end
-		end
-		#eval("vCar = #{regi}.clone") #or.. puts eval(#{reg}.make)
+		londonBranch.car_details(regnum)
+		gets
 	when 9
 		exit
 	else
 		puts "Didnt understand that, press enter to go back to the menu again"
 		blah = gets.chomp
-		mainmenu
+		mainmenu(londonBranch)
 	end
-
-#mainmenu #turn this on for live
+	mainmenu(londonBranch) #turn this on for live
 end
 
-mainmenu
+def start
+	londonBranch = CarDealer.new
+	mainmenu(londonBranch)
+end
+start
