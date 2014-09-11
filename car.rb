77d28853eb2f00335761607cc8c9909802460943
abdhass <hassgt@gmail.com>
2014-09-11 17:23:26 +0100

    Final Car Class and CarDealer Class

--- car.rb
+++ car.rb
@@ -6,21 +6,31 @@ class Car
     @cost = cost
   end
   def description
-  puts "This car is a #{@make} and costs #{@cost}"
+    puts "This car is a #{@make} and costs #{@cost}"
   end
 end
 
 class CarDealer
-    @@cars = []
+  @@cars = []
+
   def newCar(reg, make, cost)
     @@cars << Car.new(reg, make, cost)
   end
-
-def show_cars
-    @@cars.each {|car| car.description} 
+  
+  def show_car
+    puts @@cars.any? ? @@cars.each {|car| car.description} : "Sorry no cars for sale"
+  end
+  
+  def car_details(reg)
+    @@cars.each do |car| 
+      if car.reg == reg
+        puts "Car Reg: #{car.reg}
+              Car Make: #{car.make}
+              Car Cost: #{car.cost}" 
+      end
+    end
   end
 end
-
 #londonBranch = CarDealer.new
 #londonBranch.newCar("LX14", "Ford", "1200")
 #londonBranch.newCar("LX13", "VW", "1200")
